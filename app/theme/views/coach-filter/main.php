842f85a1fa4661692a07f09be2a228afcfcf672c
Alireza <website.khosravi@gmail.com>
2022-05-15 15:15:52 +0430

    responsive coach page and filter fix

--- app/theme/views/coach-filter/main.php
+++ app/theme/views/coach-filter/main.php
@@ -19,7 +19,7 @@ use App\theme\services\CoachPostMeta;
 	<?php if ( $coach_query->have_posts() ): ?>
 		<?php while ( $coach_query->have_posts() ): $coach_query->the_post(); ?>
 
-            <div class="uk-width-1-3 uk-text-center">
+            <div class="uk-width-1-2 uk-width-1-3@m uk-text-center">
                 <a class="uk-text-decoration-none" href="<?php echo get_the_permalink() ?>">
                     <img style="height: 170px!important;" src="<?php echo esc_url( CoachPostMeta::get_coach_property( get_the_ID() )["coach_image"] ) ?>" class="uk-border-circle f1-border-3 f1-border-solid f1-border-primary uk-object-cover" width="170">
                     <p class="uk-text-bold f1-text-black uk-margin-small-top"><?= get_the_title() ?></p>
--- app/theme/views/coach-page/wrapper.php
+++ app/theme/views/coach-page/wrapper.php
@@ -13,7 +13,7 @@ use App\utils\View;
 <section class="uk-section uk-section-small uk-background-muted">
     <div class="uk-container">
         <div uk-grid>
-            <div class="uk-width-1-5">
+            <div class="uk-width-1-1 uk-width-1-5@m">
 				<?php View::render( "app.theme.views.coach-page.sidebar" ); ?>
             </div>
             <div class="uk-width-expand">
